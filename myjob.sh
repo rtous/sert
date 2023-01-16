11b7f1a19009f14c3ad68172c0ee37b77d3ff67e
Ruben Tous <ruben.tous@adsmurai.com>
2023-01-16 16:03:50 +0100

    nc

--- myjob.sh
+++ myjob.sh
@@ -5,16 +5,7 @@ set -x #echo on
 #SBATCH --output=/scratch/nas/4/rtous/sert/sortida-%j.out
 #SBATCH --error=/scratch/nas/4/rtous/sert/error-%j.out
 
-#CSCRATCH=/scratch/nas/4/`whoami`
-
-#PROJECT_NAME=sert
-
-#CURRENT_ENVIRONMENT=`ls -d $CSCRATCH`/$PROJECT_NAME
-
-#source $CURRENT_ENVIRONMENT/bin/activate
-
-echo 
-source /scratch/nas/4/rtous/myvenv/bin/activate
+source /scratch/nas/4/rtous/sert/myvenv/bin/activate
 
 python cuda.py
 
